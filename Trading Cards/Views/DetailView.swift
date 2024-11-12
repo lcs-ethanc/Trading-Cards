e9888e8ab2f0294301d2815fdc5fbff1d6f51c9d
Ethan Chen <echen@lcs.on.ca>
2024-11-12 15:24:40 -0500

    finsihed info, abilities, started roundedrectangle

--- Trading Cards/Views/DetailView.swift
+++ Trading Cards/Views/DetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
             Text("Kid Goku")
                 .font(.system(size: 50))
                 .fontWeight(.bold)
@@ -22,10 +22,17 @@ struct DetailView: View {
                 .background(Color.white)
                 .padding(5)
                 .background(Color.gray)
-            Text("Stats:")
-                .font(.system(size: 30))
-                .fontWeight(.semibold)
-                .padding(.bottom,1)
+            ZStack{
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(Color.yellow)
+                    .frame(width:100,height:50)
+                Text("Stats:")
+                    .font(.system(size: 30))
+                    .fontWeight(.semibold)
+                    .padding(.bottom,1)
+
+            }
+
             HStack{
                 VStack(alignment: .leading){
                     Text("Power: 10")
@@ -42,15 +49,33 @@ struct DetailView: View {
                     Text("Battle IQ: 15")
                 }
             }
+            .fontWeight(.bold)
             .padding(.bottom,5)
             Text("Info:")
                 .font(.system(size: 30))
                 .fontWeight(.regular)
                 .padding(.bottom,1)
             HStack{
-                
+                VStack(alignment: .leading){
+                    Text("Series: Dragon Ball")
+                    Text("Species: Saiyan")
+                }
+
+                .padding(.trailing,10)
+                VStack(alignment: .leading){
+                    Text("School: Turtle School")
+                    Text("Power Level: 10-260")
+                }
             }
             .padding(.bottom,1)
+            Text("Abilities:")
+                .font(.system(size: 30))
+                .fontWeight(.regular)
+                .padding(.bottom,1)
+            Text("Kamehameha: A powerful energy wave")
+            Text("Power Pole: A magical staff that can extend to incredible lengths.")
+
+
         }
     }
 }
