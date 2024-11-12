191435c88cb843226d0f9a4bd5a9edc0354ad4de
Ethan Chen <echen@lcs.on.ca>
2024-11-12 14:15:18 -0500

    Initial Commit

---
+++ Trading Cards/Trading_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_CardsApp.swift
+//  Trading Cards
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct Trading_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
